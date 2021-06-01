7ee1d922ab6abfe8dfa3cd7cac4dc1f6b5940627
Martin <martin.britez@gmail.com>
2021-06-01 20:05:11 -0300

    commit3

--- test/ar.edu.unlam.pb2.eva01/BibliotecaTest.java
+++ test/ar.edu.unlam.pb2.eva01/BibliotecaTest.java
@@ -3,14 +3,32 @@ package ar.edu.unlam.pb2.eva01;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaTest {
 
     @Test
     public void existeBiblioteca(){
-        Libro libro = new Geografia("Argentina",1);
+        Estudiante estudiante = new Estudiante("Martin","Britez",36822159);
 
-        Biblioteca biblioteca = new Biblioteca("Biblioteca Nacional");
+        List<Libro> libros = new ArrayList<>();
+        Libro libro1 = new Geografia("Argentina",1,"Pepe");
+        Libro libro2 = new Matematica("Numeros",1,"Pepe");
+        Libro libro3 = new Historia("San Martin",1,"Pepe");
+
+        libros.add(libro1);
+        libros.add(libro2);
+        libros.add(libro3);
+
+        List<Prestamo> prestamos = new ArrayList<>();
+        Prestamo prestamo = new Prestamo(1,estudiante,libros);
+        prestamos.add(prestamo);
+
+        Biblioteca biblioteca = new Biblioteca("Biblioteca Nacional",libros,prestamos);
 
         Assert.assertNotNull(biblioteca);
     }
+
+    public void
 }
--- test/ar.edu.unlam.pb2.eva01/PrestamoTest.java
+++ test/ar.edu.unlam.pb2.eva01/PrestamoTest.java
@@ -1,10 +1,21 @@
 package ar.edu.unlam.pb2.eva01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrestamoTest {
 
     public void existePrestamo() {
         Estudiante estudiante = new Estudiante("Martin","Britez",36822159);
-        Prestamo prestamo = new Prestamo(1 ,estudiante,);
+        List<Libro> libros = new ArrayList<>();
+        Libro libro1 = new Geografia("Argentina",1,"Pepe");
+        Libro libro2 = new Matematica("Numeros",1,"Pepe");
+        Libro libro3 = new Historia("San Martin",1,"Pepe");
+
+        libros.add(libro1);
+        libros.add(libro2);
+        libros.add(libro3);
+        Prestamo prestamo = new Prestamo(1 ,estudiante,libros);
 
 
 
